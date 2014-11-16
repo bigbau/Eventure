b03b295f960d47df92352fe955bd721549b1c3b4
bigbau <julius_bautista@rocketmail.com>
2014-11-16 23:05:59 +0800

    Eventure Build 1.1

--- src/Eventure.java
+++ src/Eventure.java
@@ -1,20 +1,29 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 import gate.*;
 import gate.gui.MainFrame;
 import gate.util.GateException;
+import gate.util.persistence.PersistenceManager;
 
 
 public class Eventure {
 	
 	
 	public static void main(String[] args) throws GateException{
-		
 		Gate.init();
 		MainFrame.getInstance().setVisible(true);
-		Factory.newDocument(stringToFile());
+		try {
+			CorpusController application = (CorpusController)PersistenceManager.loadObjectFromFile(new File("src/Eventure.gapp"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Document testDocu = Factory.newDocument(stringToFile());
+		Corpus testCorpus = Factory.newCorpus("Eventure Corpus");
+		testCorpus.add(testDocu);
 		//System.out.println(Gate.getPluginsHome());
 	}
 	
