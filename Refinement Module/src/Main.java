cec99858455c5c7f36b9d71fdc7ae220578b2c4a
arjaylc <arjaycal@gmail.com>
2014-11-17 14:19:42 +0800

    Refinement Module
    Can now identify Wordnet synonyms

--- Refinement Module/src/Main.java
+++ Refinement Module/src/Main.java
@@ -11,7 +11,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        wordnet.WordnetProcessor.printWordData("give", POS.VERB);
+        wordnet.WordnetProcessor.printSynonyms("dog", POS.NOUN);
+        System.out.println(wordnet.WordnetProcessor.areSynonyms("dog","Canis familiaris", POS.NOUN));
+        
     }
     
 }
--- Refinement Module/src/wordnet/WordnetProcessor.java
+++ Refinement Module/src/wordnet/WordnetProcessor.java
@@ -4,15 +4,18 @@ import edu.mit.jwi.Dictionary;
 import edu.mit.jwi.IDictionary;
 import edu.mit.jwi.item.IIndexWord;
 import edu.mit.jwi.item.IPointer;
+import edu.mit.jwi.item.ISynset;
 import edu.mit.jwi.item.IWord;
 import edu.mit.jwi.item.IWordID;
 import edu.mit.jwi.item.POS;
 import edu.mit.jwi.item.Pointer;
 import edu.mit.jwi.morph.IStemmer;
+import edu.mit.jwi.morph.SimpleStemmer;
 import edu.mit.jwi.morph.WordnetStemmer;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -24,8 +27,52 @@ import java.util.logging.Logger;
 public abstract class WordnetProcessor {
     final static String path = "dict";
     static IDictionary dict = null;
-    public static boolean isSynonym(String word1, String word2, POS pos){
+    public static boolean areSynonyms(String word1, String word2, POS pos){
+        ArrayList<String> synonyms = getSynonyms(word1, pos);
+        SimpleStemmer stemmer= new SimpleStemmer();
+        for(String s: synonyms){
+            if(normalize(s).toLowerCase().equals(normalize(word2).toLowerCase())){
+                return true;
+            }
+        }
         return false;
+    }
+    public static String normalize(String word){
+        word=word.trim();
+        if(word.length()==0){
+            throw new IllegalArgumentException();
+        }
+        for(int i = 0; i<word.length(); i++){
+            if (Character.isWhitespace(word.charAt(i))){
+                word = word.replaceAll(" ", "_");
+                break;
+            }
+        }
+        return word;
+    }
+    public static void printSynonyms(String word, POS pos){
+        ArrayList<String> synonyms = getSynonyms(word, pos);
+        System.out.println("Printing synonyms for word \""+word+"\"...");
+        for(String s: synonyms){
+            System.out.println(s);
+        }
+    }
+    public static ArrayList<String> getSynonyms(String word, POS pos) {
+        openDictionary();
+        // look up first sense of the word "dog "
+        IIndexWord idxWord = dict.getIndexWord(word, pos);
+        ArrayList<String> synonyms = new ArrayList<String>();
+        for (int i = 0; i < idxWord.getWordIDs().size(); i++) {
+            IWordID wordID = idxWord.getWordIDs().get(i); // curr meaning
+            IWord wnWord = dict.getWord(wordID);
+            ISynset synset = wnWord.getSynset();
+            
+            // iterate over words associated with the synset
+            for (IWord w : synset.getWords()) {
+                synonyms.add(w.getLemma());
+            }
+        }
+        return synonyms;
     }
     public static String findRoot(String word, POS pos){
         openDictionary();
