13d447fdb684e190343f2c9988adea11789fdd69
bigbau <julius_bautista@rocketmail.com>
2014-11-18 00:47:19 +0800

    Eventure Build 1.2

--- src/Eventure.java
+++ src/Eventure.java
@@ -1,22 +1,56 @@
+import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Set;
 
 import gate.*;
+import gate.creole.ResourceInstantiationException;
 import gate.gui.MainFrame;
+import gate.persist.PersistenceException;
 import gate.util.GateException;
 import gate.util.persistence.PersistenceManager;
 
 
 public class Eventure {
-	
-	
-	public static void main(String[] args) throws GateException{
+
+
+	private static List extractedAnnotations;
+	private static CorpusController application;
+	private static MainWindow eventure = new MainWindow();
+
+	public static void main(String[] args){
+		eventure.initialize();
+		eventure.addBtnRunActionListener(new ActionListener(){
+
+			public void actionPerformed(ActionEvent e)
+			{
+				try {
+					run();
+				} catch (GateException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+
+			}
+		});
+
+	}
+
+	public static void run() throws GateException{
 		Gate.init();
-		MainFrame.getInstance().setVisible(true);
+		//MainFrame.getInstance().setVisible(true);
 		try {
-			CorpusController application = (CorpusController)PersistenceManager.loadObjectFromFile(new File("src/Eventure.gapp"));
+			application = (CorpusController)PersistenceManager.loadObjectFromFile(new File("src/Eventure.gapp"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -24,12 +58,49 @@ public class Eventure {
 		Document testDocu = Factory.newDocument(stringToFile());
 		Corpus testCorpus = Factory.newCorpus("Eventure Corpus");
 		testCorpus.add(testDocu);
-		//System.out.println(Gate.getPluginsHome());
-	}
-	
-	
-	public static String stringToFile(){
+		application.setCorpus(testCorpus);
+
+		application.execute();
+		String docXMLString = null; 
+
+		List annotTypesToWrite = new ArrayList();
+		annotTypesToWrite.add("EffectOf");
+		annotTypesToWrite.add("EffectOfIsState");
+		annotTypesToWrite.add("CauseOfIsState");
+		annotTypesToWrite.add("HappensRelation");
+		annotTypesToWrite.add("EventForGoalEvent");
+		annotTypesToWrite.add("EventForGoalState");;
+		// if we want to just write out specific annotation types, we must
+		// extract the annotations into a Set
+
+		// Create a temporary Set to hold the annotations we wish to write out
+		Set annotationsToWrite = new HashSet();
+
+		// we only extract annotations from the default (unnamed) AnnotationSet
+		// in this example
+		AnnotationSet defaultAnnots = testDocu.getAnnotations();
+
+		Iterator annotTypesIt =  annotTypesToWrite.iterator();
+
+		while(annotTypesIt.hasNext()) {
+			// extract all the annotations of each requested type and add them to
+			// the temporary set
+			AnnotationSet annotsOfThisType = defaultAnnots.get((String)annotTypesIt.next());
+			if(annotsOfThisType != null) {
+				annotationsToWrite.addAll(annotsOfThisType);
+			}
+		}
+
+		// create the XML string using these annotations
+		//eventure.setMessageArea(annotationsToWrite.toString());
+		extractedAnnotations = new ArrayList(annotationsToWrite);
 		
+		for(int i = 0; i<extractedAnnotations.size();i++)
+			eventure.addMessageArea(extractedAnnotations.get(i)+"\n");
+
+	}
+	public static String stringToFile(){
+
 		String content = "";
 		try {
 			content = new Scanner(new File("src/sample story.txt")).useDelimiter("\\Z").next();
---
+++ src/MainWindow.java
@@ -0,0 +1,106 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import java.awt.Color;
+import java.awt.event.ActionListener;
+
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+
+
+public class MainWindow extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField inputLocation;
+	private JTextArea messageArea;
+	private JButton btnBrowse;
+	private JButton btnRun;
+	private JScrollPane scrollPane;
+	/**
+	 * Launch the application.
+	 */
+	/**
+	 * Create the frame.
+	 */
+	public void initialize(){
+		this.setVisible(true);
+	}
+	
+	public MainWindow() {
+		setResizable(false);
+		setFont(new Font("Arial", Font.PLAIN, 12));
+		setTitle("Eventure Build 1.2");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblEventure = new JLabel("EVENTURE");
+		lblEventure.setBounds(103, 11, 574, 23);
+		lblEventure.setHorizontalAlignment(SwingConstants.CENTER);
+		lblEventure.setFont(new Font("Arial", Font.PLAIN, 19));
+		contentPane.add(lblEventure);
+		
+		inputLocation = new JTextField();
+		inputLocation.setText("Click browse to look for a .txt file");
+		inputLocation.setBounds(10, 45, 640, 29);
+		inputLocation.setEditable(false);
+		inputLocation.setFont(new Font("Arial", Font.ITALIC, 13));
+		contentPane.add(inputLocation);
+		inputLocation.setColumns(10);
+		
+		btnBrowse = new JButton("Browse");
+		btnBrowse.setBounds(660, 45, 124, 29);
+		btnBrowse.setFont(new Font("Arial", Font.PLAIN, 14));
+		btnBrowse.setBackground(Color.WHITE);
+		contentPane.add(btnBrowse);
+		
+		btnRun = new JButton("RUN");
+		
+		btnRun.setFont(new Font("Arial", Font.PLAIN, 14));
+		btnRun.setBackground(Color.WHITE);
+		btnRun.setBounds(10, 85, 774, 29);
+		contentPane.add(btnRun);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 121, 774, 430);
+		contentPane.add(scrollPane);
+		
+		messageArea = new JTextArea();
+		scrollPane.setViewportView(messageArea);
+		messageArea.setEditable(false);
+		
+	}
+	public void addBtnBrowseActionListener(ActionListener al) {
+		btnBrowse.addActionListener(al);
+	}
+	public void addBtnRunActionListener(ActionListener al) {
+		btnRun.addActionListener(al);
+	}
+	public String getInputLocationText() {
+		return inputLocation.getText();
+	}
+	public void setMessageArea(String messages) {
+		messageArea.setText(messages);
+	}
+	public void addMessageArea(String messages) {
+		messageArea.append(messages);
+	}
+}
